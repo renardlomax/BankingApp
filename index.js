ff4f128961294327de9587b34c3a13ae75678c74
Renard Lomax <76239901+renardlomax@users.noreply.github.com>
2021-11-08 23:01:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function Spa() {
      <div id="data">
 
 
-     <UserContext.Provider value={{users:[{name:'Renard Lomax',email:'renardlomax1990@gmail.com',password:'secret',balance:900}]}}>
-        <div className="container" style={{padding: "20px" }}>
+     <UserContext.Provider value={{users:[{name:'Renard',email:'renardlomax1990@gmail.com',password:'secret',balance:9000},
+                                  {name:'Bill',email:'Bill@gmail.com',password:'12345678',balance:300},
+                                  {name:'Steve',email:'steve@gmail.com',password:'secret12',balance:7200
+                                  },{name:'test',email:'test',password:'test',balance:7200
+                                }]}}>
+        
+        
+        \<div className="container" style={{padding: "20px" }}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
@@ -14,9 +20,10 @@ function Spa() {
           <Route path="/withdraw/" component={Withdraw} />
           <Route path="/balance/" component={Balance} />
           <Route path="/alldata/" component={AllData} />
+          {/* <Route path="/transfer/" component={Transfer} /> */}
         </div>
       </UserContext.Provider>  
-
+      
 
      </div>
          
