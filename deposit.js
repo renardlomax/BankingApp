90e9ad8902fc2dc131d8caf54c4e0a7ee22dd07e
Renard Lomax <76239901+renardlomax@users.noreply.github.com>
2021-11-08 21:45:49 -0500

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,59 +1,134 @@
-function Deposit(){
-  const [show, setShow] = React.useState(true);
-  const [total, setTotal] = React.useState(100);
-  const [deposit, setDeposit] = React.useState('');
-  const [status, setStatus] = React.useState('');
-  const ctx = React.useContext(UserContext); 
+function Deposit() {
+	const [show, setShow]                   = React.useState(true);
+	const [disabled, setDisabled]           = React.useState(true);
+	const [status, setStatus]   	        = React.useState('');
+	const [currentuser, setCurrentuser]     = React.useState('');
+	const [currentemail, setCurrentemail]   = React.useState('');
+	const [currentpass, setCurrentpass]     = React.useState('');
+	const [balance, setBalance]				= React.useState('');
+	const [deposit, setDeposit]				= React.useState('');
+	const ctx = React.useContext(UserContext);
+	
+	//Check if any user is currently logged in
+	if (show) {
+		for (const {name, email, password, balance, loggedin} of ctx.users) {
+			console.log(`Current User: ${name}, Logged in: ${loggedin}`);
+			if (loggedin) {
+				setShow(false);
+				setCurrentuser(name);
+				setCurrentemail(email);
+				setCurrentpass(password);
+				setBalance(balance);
+				console.log(`${name} is logged in`);
+				return;
+			}
+		}
+	}
+	
+	//Determine if to set the button disabled or not
+	if (!deposit) {
+		//Check if button should be enabled
+		if (disabled) {
+			console.log(disabled);
+			console.log(`button disabled ${disabled}`);
+		} else {
+			setDisabled(true);
+			console.log(`button disabled ${disabled}`);
+		}
+	} else {
+		if (disabled) {
+			setDisabled(false);
+			console.log(`button disabled ${disabled}`);
+		} else {
+			console.log(`button disabled ${disabled}`);
+		}
+	}
 
-  function balance(){
-     if (deposit.includes('-') || deposit.includes('.')){
-       let new_deposit = deposit.replace('-', '');
-       new_deposit = new_deposit.replace('.', '');
-       if (!(/^\d+$/.test(new_deposit))){
-        alert('Only enter numbers');
-        return;
-      }
-     } else {
-      if (!(/^\d+$/.test(deposit))){
-        alert('Only enter numbers');
-        return;
-      }
-     }
-     if (Number(deposit) < 0) {
-       alert('Only enter positive numbers');
-       return
-     } 
-     setTotal(total + Number(deposit))
-     setStatus('Your deposit was successful.')
-     setTimeout(() => setStatus(''),3000);
-  } 
-  return (
-   
-    <div className="text-center">
-    <Card
-    header="Account"
-    bgcolor="danger"
-    header="Login"
-    status={status}
-    body={  (
-       <>
-      <div><h3>
-        
-        Balance: ${total}
-        </h3></div>
-      
-      
-      <br/>
-       Deposit:
-       <input autoComplete="off" type="text" className="form-control" id="deposit" placeholder="Enter Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} /><br/>
-      <button type="submit" className="btn btn-light" id='deposit-button' onClick={balance} disabled={!deposit}>Deposit</button>
-      </> 
-    )} 
-    
-    
-      
-      
-    />
-  </div>
-  )
+	function depositMoney() {
+		if (!isNaN(deposit) && deposit > 0){
+			let newBalance = Number(balance) + Number(deposit);
+			console.log(Number(newBalance));
+			let tracker = false;
+			
+			// For todays date;
+			// Date.prototype.today = function () { 
+			// 	return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+			// }
+	
+			// var newDate = new Date();
+			
+			//Check if the username or passwords match anyone is the database
+			for (const {email, password, balance} of ctx.users) {
+				console.log(`Checking ${currentemail} ${currentpass} against ${email} ${password}`);
+				if (currentemail == email && currentpass == password) {
+					console.log(`${currentemail} ${currentpass} is correct`);
+					for (var i=0, length=ctx.users.length; i<length; i++) {
+						if (ctx.users[i].email == currentemail) {
+							console.log(`Checking ${email}`)
+							ctx.users[i].balance = Number(newBalance);
+							// var datetime = `Deposited $${deposit} on ${newDate.today()}`;
+							// ctx.users[i].submissions.push(datetime);
+							tracker = true;
+						}
+					}
+				}
+			}
+			
+			//Making all changes to state
+			if (tracker) {
+				setStatus(`$${deposit} successfully deposited into account`);
+				setTimeout(() => setStatus(''), 5000);
+				setDeposit('');
+				setBalance(Number(newBalance))
+			} 
+		} else if (!isNaN(deposit)) {
+			setStatus('Error: Deposit amount must be greater than $0.00. Please try again.');
+			setDeposit('');
+			setTimeout(() => setStatus(''), 5000);
+		} else {
+		setStatus('Error: Deposit amount must be a number. Please try again.');
+		setDeposit('');
+		setTimeout(() => setStatus(''), 5000);
+	}
+		return;
+	}
+	
+	return (
+		<Card
+			bgcolor="danger"	
+			txtcolor="dark"
+			header = "Deposit funds in your account"
+			status = {status}
+			body = {show ? (
+				<>
+          <h3>Please log in to deposit funds</h3>
+				 <a href='#/login/' className="btnDeposit" data-toggle="tooltip" title="Login to your account">Login</a><br/><br/><br/>
+				You don't have an account? Create an account if you would like to enjoy our services. <a href="#/createaccount/" className="btnDeposit" data-toggle="tooltip" title="Create a new account">Create Account</a> <br/><br/>
+				</>
+			):(
+				<>
+				Current Balance: ${balance.toFixed(2)}<br/><br/>
+				
+				Deposit Amount:<br/>
+				<input type="number" className="form-control" id="deposit" placeholder="Deposit Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)}/><br/>
+				{disabled ? (
+					<>
+					<button type="submit" className="btn btn-primary" disabled="disabled" onClick={depositMoney}>Deposit</button>
+					<br/>
+					<a href="#withdraw" class="button">Withdraw</a><br/>
+					<br/> 
+					<a href="#alldata" class="button">your transactions</a>
+					
+					</>
+				):(
+					<>
+					<button type="submit" className="btn btn-primary" onClick={depositMoney}>Deposit</button><br/><br/> 
+					
+					
+					</>
+				)}
+				</>
+			)}
+		/>
+	)
 }
